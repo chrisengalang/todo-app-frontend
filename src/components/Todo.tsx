fbfaa0fc559e1eed3ba8c5eb196311d875d02e06
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-09-25 13:19:27 -0700

    Toggle of todo item

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Form, ListGroup } from "react-bootstrap"
 
 interface Props {
@@ -7,9 +8,27 @@ interface Props {
 }
 
 const Todo = ({id, name, done}: Props) => {
+
+  const [isDone, setIsDone] = useState(done)
+
+  const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = e.target.checked
+
+    fetch('http://localhost:8080/todos', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({id: id, name: name, done: isChecked})
+    })
+      .then(response => response.json())
+      .then(data => setIsDone(data.done))
+  }
+
   return <>
     <ListGroup.Item>
-      <Form.Check type='checkbox' defaultChecked={done} label={name} />
+      <Form.Check type='checkbox' defaultChecked={isDone} label={name} onChange={handleToggle} />
     </ListGroup.Item>
   </>
 }
